e5094d4f5c94638fc6768a16b44a622f1a07cbd8
Fred Gleason <fredg@paraelsystems.com>
2017-03-04 16:24:53 -0500

    2017-03-04 Fred Gleason <fredg@paravelsystems.com> 	* Initial package creation.

---
+++ src/config.h
@@ -0,0 +1,37 @@
+// config.h
+//
+// Configuration for ttylogger(8)
+//
+//   (C) Copyright 2017 Fred Gleason <fredg@paravelsystems.com>
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as
+//   published by the Free Software Foundation; either version 2 of
+//   the License, or (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public
+//   License along with this program; if not, write to the Free Software
+//   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <QString>
+
+#define CONFIG_FILE "/etc/ttylogger.conf"
+
+class Config
+{
+ public:
+  Config();
+  bool load();
+};
+
+
+#endif  // CONFIG_H
